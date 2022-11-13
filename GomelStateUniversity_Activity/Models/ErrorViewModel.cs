24a29e4f55d3f25e42a0d137a79649ef3bea23d4
Dmitry Guznenok <medsia@mail.ru>
2022-11-13 20:01:35 +0300

    Добавьте файлы проекта.

---
+++ GomelStateUniversity_Activity/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GomelStateUniversity_Activity.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
