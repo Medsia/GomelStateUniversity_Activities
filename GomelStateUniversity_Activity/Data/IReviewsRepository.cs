a5ffff8ab5e35665525f0c912abcb23a87b6dc5b
Andrei Shumilov <andrei1shum@mail.ru>
2022-11-20 18:42:06 +0300

    FIxed CreateReviewAsync and UpdateReviewAsync methods

--- GomelStateUniversity_Activity/Data/IReviewsRepository.cs
+++ GomelStateUniversity_Activity/Data/IReviewsRepository.cs
@@ -10,8 +10,8 @@ namespace GomelStateUniversity_Activity.Data
         public Task<IEnumerable<Models.Review>> GetReviewsAsync();
         public Task<IEnumerable<Models.Review>> GetMyReviewsAsync(string userId);
         public Task<Models.Review> GetReviewAsync(int id);
-        public Task CreateReviewAsync(IFormCollection form, DateTime dateTime);
-        public Task UpdateReviewAsync(IFormCollection form, DateTime dateTime);
+        public Task CreateReviewAsync(IFormCollection form);
+        public Task UpdateReviewAsync(IFormCollection form);
         public Task DeleteReviewAsync(int id);
     }
 }
