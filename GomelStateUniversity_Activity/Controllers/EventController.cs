4e892f683020088c5ecb11bacbcb80ab907db110
Dmitry Guznenok <medsia@mail.ru>
2022-11-26 15:59:56 +0300

    Add subdivisions Data and appForm model

--- GomelStateUniversity_Activity/Controllers/EventController.cs
+++ GomelStateUniversity_Activity/Controllers/EventController.cs
@@ -213,7 +213,7 @@ namespace GomelStateUniversity_Activity.Controllers
             {
                 ViewData["Message"] = TempData["Message"];
             }
-            int CultureSubdivisionId = 2;
+            int CultureSubdivisionId = 1;
             return View(await _eventRepository.GetEventsBySubdivisionAsync(CultureSubdivisionId));
         }
         public async Task<IActionResult> Sport()
@@ -222,7 +222,7 @@ namespace GomelStateUniversity_Activity.Controllers
             {
                 ViewData["Message"] = TempData["Message"];
             }
-            int SportSubdivisionId = 3;
+            int SportSubdivisionId = 2;
             return View(await _eventRepository.GetEventsBySubdivisionAsync(SportSubdivisionId));
         }
         public async Task<IActionResult> MassEvents()
@@ -232,7 +232,7 @@ namespace GomelStateUniversity_Activity.Controllers
             {
                 ViewData["Message"] = TempData["Message"];
             }
-            int MassEventsSubdivisionId = 4;
+            int MassEventsSubdivisionId = 3;
             return View(await _eventRepository.GetEventsBySubdivisionAsync(MassEventsSubdivisionId));
         }
         public async Task<IActionResult> Volunteers()
@@ -241,7 +241,7 @@ namespace GomelStateUniversity_Activity.Controllers
             {
                 ViewData["Message"] = TempData["Message"];
             }
-            int VolunteersSubdivisionId = 12;
+            int VolunteersSubdivisionId = 4;
             return View(await _eventRepository.GetEventsBySubdivisionAsync(VolunteersSubdivisionId));
         }
         
---
+++ GomelStateUniversity_Activity/Data/Migrations/20221126124701_SubdivisionData.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GomelStateUniversity_Activity.Data.Migrations
+{
+    public partial class SubdivisionData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Subdivisions",
+                columns: new[] { "Id", "Contacts", "Name" },
+                values: new object[,]
+                {
+                    { 1, "VELIKY@gsu.by", "Отдел культуры и досуга молодежи" },
+                    { 2, "KULESHOV@gsu.by", "Спортивный клуб" },
+                    { 3, "LVDUBROVSKAYA@gsu.by", "Информационно-аналитическая служба и отдел воспитательной работы с молодежью" },
+                    { 4, "FEDORENKO@gsu.by", "Трудовая и волонтерская деятельность" },
+                    { 5, "AZYAVCHIKOV@gsu.by", "Трудовая и волонтерская деятельность" },
+                    { 6, "TROSHEVA@gsu.by", "Консультации психолога" },
+                    { 7, "hodanovich@gsu.by", "Отзывы" },
+                    { 8, "osnach@gsu.by", "Отзывы" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 6);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                table: "Subdivisions",
+                keyColumn: "Id",
+                keyValue: 8);
+        }
+    }
+}
